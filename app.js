d28d2fd69703c9e255c6347ee057e9d56aaec7ce
Akramwired <kahd.unplugged@gmail.com>
2020-10-02 20:14:30 +0600

    mongo atlas connected

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ express        = require("express"),
 app            = express();
 
 // APP CONFIG
-mongoose.connect("mongodb://localhost:27017/restful_blog_app", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.DATASET, {useNewUrlParser: true, useUnifiedTopology: true});
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
